a952027570b1e98f6462a10cf1ce73ddf20f4775
Vladimir Tyrin <b0-0b@yandex.ru>
2017-06-24 22:12:19 +0300

    Sample added

--- VirtualMemberTest/Program.cs
+++ VirtualMemberTest/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VirtualMemberTest
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            new Child();
+        }
+    }
+
+    internal class Parent
+    {
+        public Parent()
+        {
+            Foo();
+        }
+
+        public virtual void Foo()
+        {
+            Console.WriteLine("Hello from parent");
+        }
+    }
+
+    internal class Child : Parent
+    {
+        private readonly string _world;
+
+        public Child()
+        {
+            _world = "WORLD";
+        }
+
+        public override void Foo()
+        {
+            Console.WriteLine($"Hello, {_world.ToLowerInvariant()}");
         }
     }
 }
